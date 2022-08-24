208823f47a2679c16fdd8288e0f11f15cd47d212
Nandy7777 <nandy7777@ukr.net>
2022-08-25 00:39:26 +0300

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+const startBtn = document.querySelector(`button[data-start]`);
+const stopBtn = document.querySelector(`button[data-stop]`);
+
+startBtn.addEventListener(`click`, onStartBtnClick);
+stopBtn.addEventListener(`click`, onStopBtnClick);
+
+const changeColor = {
+    intervalId: null,
+    start() {
+        startBtn.disabled = true;
+        stopBtn.disabled = false;
+        this.intervalId = setInterval(() => {
+            document.body.style.backgroundColor = getRandomHexColor();
+        }, 1000);
+    }, 
+    stop() {
+        clearInterval(this.intervalId);
+        startBtn.disabled = false;
+        stopBtn.disabled = true;
+    }
+};
+       
+function onStartBtnClick() {
+    changeColor.start();
+}
+
+function onStopBtnClick(evt) {
+    changeColor.stop();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
