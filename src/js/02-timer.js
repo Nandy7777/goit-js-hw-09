9a5117f83c315aaebc4dc69cd5ff7696967864da
Nandy7777 <nandy7777@ukr.net>
2022-08-27 19:21:52 +0300

    2 завдання все, крім clear interval

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,8 +1,19 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
 
 const startBtn = document.querySelector(`button[data-start]`);
-const clockFace = document.querySelector(`.timer`);
+const input = document.querySelector('#datetime-picker');
+const spanDays = document.querySelector('span[data-days]');
+const spanHours = document.querySelector('span[data-hours]');
+const spanMinutes = document.querySelector('span[data-minutes]');
+const spanSeconds = document.querySelector('span[data-seconds]');
+
+startBtn.setAttribute('disabled', 'true');
+
+let inrervaId = null;
+let deltaTime = null;
+
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -10,78 +21,59 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     console.log(selectedDates[0]);
+    if (selectedDates[0].getTime() < options.defaultDate.getTime()) { 
+      return Notiflix.Notify.failure('Please choose a date in the future');
+    } else {
+      startBtn.removeAttribute('disabled');
+    };
   },
 };
 
-const input = document.querySelector('#datetime-picker');
-const fp = flatpickr(input, {options});  // flatpickr
+flatpickr('#datetime-picker', options);
 
+startBtn.addEventListener(`click`, onStartBtnClick);
 
+function onStartBtnClick() {
+  inrervaId = setInterval(() => {
+    startBtn.disabled = true;
+    deltaTime = new Date(input.value) - new Date();
+    const { days, hours, minutes, seconds } = convertMs(deltaTime);
+      spanDays.textContent = `${days}`;
+      spanHours.textContent = `${hours}`;
+      spanMinutes.textContent = `${minutes}`;
+      spanSeconds.textContent = `${seconds}`;
+    if (deltaTime <= 0) {
+      clearInterval(inrervaId);
+      startBtn.disabled = false;
+    };
+  }, 1000);
+};
 
-class Timer {
-  constructor({ onTick }) {
-    this.inrervaId = null;
-    this.isActive = false;
-    this.onTick = onTick;
+function addLeadingZero(value) {
+  return String(value).padStart(2, `0`);
+};
 
-    this.init();
-  }
-  init() {
-    const time = this.convertMs(0);
-    this.onTick(time);
-  }
-  start() {
-    if (this.isActive) {
-      return;
-    }
-    const startTime = Date.now();
-    this.isActive = true;
-
-    this.inrervaId = setInterval(() => {
-      const currentTime = Date.now();
-      const deltaTime = currentTime - startTime;
-      const time = this.convertMs(deltaTime);
-
-      this.onTick(time);
-    }, 1000);
-  }
-
-  convertMs(ms) {
-    // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-
-    // Remaining days
-    const days = this.pad(Math.floor(ms / day));
-    // Remaining hours
-    const hours = this.pad(Math.floor((ms % day) / hour));
-    // Remaining minutes
-    const minutes = this.pad(Math.floor(((ms % day) % hour) / minute));
-    // Remaining seconds
-    const seconds = this.pad(
-      Math.floor((((ms % day) % hour) % minute) / second)
-    );
-
-    return { days, hours, minutes, seconds };
-  }
-
-  pad(value) {
-    return String(value).padStart(2, `0`);
-  }
+function padDays(value) {
+  return String(value).padStart(3, `0`);
 }
 
-const timer = new Timer({
-  onTick: updateClockFace,
-});
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
-startBtn.addEventListener(`click`, timer.start.bind(timer));
+  // Remaining days
+  const days = padDays(Math.floor(ms / day));
+  // Remaining hours
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
 
-function updateClockFace({ days, hours, minutes, seconds }) {
-  clockFace.textContent = `${days}:${hours}:${minutes}:${seconds}`;
-}
-
-// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+  return { days, hours, minutes, seconds };
+};
